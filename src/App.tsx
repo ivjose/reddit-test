20df69f89ffd72f0bdb1632efe916da9bcc53d97
ivjose <ivjosesantos@gmail.com>
2021-07-09 22:04:36 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const data = '';
+  const data = 'sample';
   return (
     <div className="App">
       <header className="App-header">
